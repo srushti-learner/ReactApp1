a94347b4acdbab377dd72b3599595f687e2dbe96
Srushti Giridhar Borate <borate_sg@ee.iitr.ac.in>
2023-11-03 16:23:41 +0530

    Update filterReducer.js

--- reducer/filterReducer.js
+++ reducer/filterReducer.js
@@ -1,15 +1,13 @@
 const filterReducer = (state, action) => {
   switch (action.type) {
     case "LOAD_FILTER_PRODUCTS":
-      // let priceArr = action.payload.map((curElem) => curElem.price);
-
-      // let maxPrice = Math.max(...priceArr);
+    
      
       return {
         ...state,
         filter_products: [...action.payload],
         all_products: [...action.payload],
-        //filters:{...state.filter,maxPrice,price:maxPrice}
+
       };
 
     case "SET_GRID_VIEW":
